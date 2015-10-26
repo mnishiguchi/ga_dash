e23e99f3cc1687c0143e46ecd367ff0222c6dd69
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-10-25 23:24:08 -0400

    Improve layout

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # ga_dash
 
-## PROJECT 1: BUILD A PERSONAL WEBSITE
+**PROJECT 1: BUILD A PERSONAL WEBSITE**
 - https://dash.generalassemb.ly/MNISHIGUCHI/build-your-own-personal-website
 
-## PROJECT 2: BUILD A RESPONSIVE BLOG THEME
+**PROJECT 2: BUILD A RESPONSIVE BLOG THEME**
 - https://dash.generalassemb.ly/MNISHIGUCHI/build-your-own-blog-theme
 
-## PROJECT 3: BUILD A SMALL BUSINESS WEBSITE
+**PROJECT 3: BUILD A SMALL BUSINESS WEBSITE**
 - https://dash.generalassemb.ly/MNISHIGUCHI/build-your-own-business-website
 
-## PROJECT 4: BUILD A CSS ROBOT
+**PROJECT 4: BUILD A CSS ROBOT**
+- https://dash.generalassemb.ly/MNISHIGUCHI/build-your-own-css-robot
 
-
-## PROJECT 5: BUILD A MADLIBS GAME
+**PROJECT 5: BUILD A MADLIBS GAME**
+- https://dash.generalassemb.ly/MNISHIGUCHI/build-your-own-madlibs-game
--- about_me/javascripts/main.js
+++ about_me/javascripts/main.js
@@ -7,21 +7,45 @@
 */
 
 
-// Ensure that DOM has already been loaded.
+// Ensure that DOM has already been loaded
 document.addEventListener('DOMContentLoaded', function() {
-
   /*
    * 1. Add click listener to all the menu items
    * 2. Toggle the class on click events
    */
-  [].forEach.call( document.querySelectorAll( '.menuItems div' ), function ( el ) {
+  [].forEach.call( document.querySelectorAll( '.sliderList div' ), function ( el ) {
       el.addEventListener( 'click', function () {
           el.classList.toggle('show-description');
       }, false );
   });
 });
+// jQuery equivalent
+// $(".sliderList div").on("click", function() {
+//   $(this).toggleClass("show-description");
+// });
 
-  // jQuery equivalent
-  // $(".menuItems div").on("click", function() {
-  //   $(this).toggleClass("show-description");
-  // });
+
+/**
+ * Opens the default email program with title, address and message prefilled.
+ */
+function sendEmail() {
+  window.location.href = "mailto:nishiguchi.masa@gmail.com"
+    + "?cc=masatoshi.nishiguchi@udc.edu"
+    + "&subject=" + escape("Hello, Masa!")
+    + "&body=" + escape(document.getElementById('message-to-masa').value)
+  ; // end window.location.href
+}
+
+/**
+ * Clear the text in the textarea.
+ */
+function clearMsg() {
+  document.getElementById('message-to-masa').value = "";
+}
+
+// Ensure that DOM has already been loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Add the click listener to send message button
+  document.getElementById("btn-send-message").addEventListener("click", sendEmail, false);
+  document.getElementById("btn-clear-message").addEventListener("click", clearMsg, false);
+});
